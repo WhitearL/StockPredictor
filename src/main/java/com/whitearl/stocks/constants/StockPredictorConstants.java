01d796e65cb92ab441835c8665d57ce6bf01dbcc
Lewis Whitear <lewis_whitear@outlook.com>
2021-12-28 00:42:36 +0000

    Stock predictor

---
+++ src/main/java/com/whitearl/stocks/constants/StockPredictorConstants.java
@@ -0,0 +1,21 @@
+package com.whitearl.stocks.constants;
+
+/**
+ * Constants for the health insurance predictor
+ * 
+ * @author WhitearL
+ *
+ */
+public class StockPredictorConstants {
+
+    /**
+     * Private constructor, prevent instantiation
+     */
+    private StockPredictorConstants() {
+
+    }
+
+    public static final String UNPROCESSED_CSV_FILE_NAME = "\\health-insurance-data.csv";
+    public static final String PROCESSED_CSV_FILE_NAME = "\\health-insurance-data-processed.csv";
+    public static final String PROCESSED_ARFF_FILE_NAME = "\\health-insurance-data-processed.arff";
+}
